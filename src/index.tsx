20c94ab09d4f1f3ee28da7c906dfe31ec425dfc5
munouse <yunho2006@naver.com>
2021-03-14 22:32:30 +0900

    feat: initial setting

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from "react"
 import ReactDom from "react-dom";
-import './index.css';
+import './styles/index.css';
 import Root from "./routes";
 
 ReactDom.render(<Root />, document.getElementById("root"));
